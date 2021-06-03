88c6775d81eeda9fc57ca69f3f7335a3def9f3be
chaitra845 <chaitrachytra@gmail.com>
2021-06-03 23:15:12 +0530

    percentage of the Singlet

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,12 +1,22 @@
 echo "Welcome to Flip Coin Combination"
 heads=1
 tails=0
+declare -A res
 
-toss=$((RANDOM%2))
+echo "Enter n for the loop "
+read n
+for (( i=0;i<n;i++ ))
+do
+   toss=$((RANDOM%2))
+   if [ $toss -eq $heads ]
+   then
+       res['heads']=$(("${res['heads']}"+1))
+   else
+       res['tails']=$(("${res['tails']}"+1))
 
-if [ $toss -eq $heads ]
-then
-   echo "Head wins"
-else
-   echo "Tail wins "
-fi
+   fi
+done
+headcnt=$(($n-${res['heads']}))
+headpercent=$((($headcnt*100)/$n))
+echo "tails=${res['tails']} and heads=${res['heads']}"
+echo "occurance of tails = $headpercent% and head = $((100-$headpercent))%" 
